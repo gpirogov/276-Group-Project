921e34bf4c23f0bafea569aa5c36a1ca5faf192b
Raymond <c9y7ip@gmail.com>
2019-06-30 04:35:40 -0700

    login system update

--- index.js
+++ index.js
@@ -4,22 +4,61 @@ const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL//,
-  //ssl : true
+  connectionString: process.env.DATABASE_URL,
+  ssl : true
 });
+
+var globalName
 
 express()
 .use(express.static(path.join(__dirname, 'public')))
 .use(express.json())
 .use(express.urlencoded({extended:false}))
+.post('/burning-plus.html', function(req,res){
+  var loginname = req.body.username;
+  var password = req.body.pw;
+  pool.query("SELECT * from user_info WHERE username = '" + loginname + "'", (err,ans) =>{
+    if ( ans.rowCount == 0)
+    {
+      res.render('pages/errorPage', {
+        msg: "No exsit account"
+      })
+    } else if ( ans.rowCount == 1 )
+    {
+      if ( password == ans.rows[0].password)
+      {
+        var username = ans.rows[0].username;
+        var gender = ans.rows[0].gender;
+        var age = ans.rows[0].age;
+        var weight = ans.rows[0].weight;
+        var height = ans.rows[0].height;
+        res.render('pages/profile', {
+              username:username,
+              age:age,
+              gender:gender,
+              weight:weight,
+              height:height,
+              status:"Beginner"
+        })
+      } else
+      {
+        res.render('pages/errorPage', {
+          msg: "OH SHIT !"
+        })
+      }
+    }
+  })
+})
 
 .post('/signup.html', function(req,res) {
   var username = req.body.username;
+  globalName = req.body.username;
   var password = req.body.pw;
   var gender = req.body.Gender;
   var age = req.body.age;
   var weight = req.body.weight;
   var height = req.body.height;
+
 
   const accountInfo = "INSERT INTO account(username,password) values ($1,$2)"
   const accountVal = [username,password]
@@ -34,91 +73,64 @@ express()
     {
       pool.query(accountInfo, accountVal);
       pool.query(userInfo,userVal);
-      res.render('pages/welcomePage.ejs')
+      res.render('pages/welcomePage')
+
     } else if (ans.rowCount == 1 )
     {
-      res.render('pages/SignUpError.ejs',{
+      res.render('pages/SignUpError',{
         msg:'Exsiting Account'
       })
     }
   })
 })
 
-.post('/:username', function(req,res){
-  var loginname = req.body.username;
-  var password = req.body.pw;
-  pool.query("SELECT * from user_info WHERE username = '" + loginname + "'", (err,ans) =>{
-    if ( ans.rowCount == 0)
-    {
-      res.render('pages/errorPage', {
-        msg: "No exsit account"
-      })
-    } else if ( ans.rowCount == 1 )
-    {
-      if ( password == ans.rows[0].password)
-      {
-        var id = ans.rows[0].id
-        res.render('pages/errorPage', {
-          msg: "CORRECT :)"
-        })
-      } else
-      {
-        res.render('pages/errorPage', {
-          msg: "OH SHIT !"
-        })
-      }
-    }
+
+.post('/beginnerChoice', function(req,res){
+  res.render('pages/questionarie')
+})
+
+.post('/beginnerQuestion', function(req,res){
+  pool.query("SELECT * FROM user_info WHERE username = '" + globalName + "'", (err,ans)=>{
+      var username = ans.rows[0].username;
+      var gender = ans.rows[0].gender;
+      var age = ans.rows[0].age;
+      var weight = ans.rows[0].weight;
+      var height = ans.rows[0].height;
+
+    res.render('pages/profile', {
+          username:username,
+          age:age,
+          gender:gender,
+          weight:weight,
+          height:height,
+    })
   })
 })
 
-// .post('/user/:id', function(req,res){
-//   console.log(req.params.id);
-//   var username = req.body.username;
-//   var password = req.body.pw;
-//   var gender = req.body.Gender;
-//   var age = req.body.age;
-//   var weight = req.body.weight;
-//   var height = req.body.height;
-//
-//   res.render('pages/profile.ejs'),{
-//     username:username,
-//     age:age,
-//     gender:gender,
-//     weight:weight,
-//     height:height,
-//     status:"Beginner"
-//   }
-// })
+.post('/experienceChoice', function(req,res){
+  res.render('pages/proquestion')
+})
+
+.post('/experienceQuestion', function(req,res){
+  pool.query("SELECT * FROM user_info WHERE username = '" + globalName + "'", (err,ans)=>{
+      var username = ans.rows[0].username;
+      var gender = ans.rows[0].gender;
+      var age = ans.rows[0].age;
+      var weight = ans.rows[0].weight;
+      var height = ans.rows[0].height;
+
+    res.render('pages/profile', {
+          username:username,
+          age:age,
+          gender:gender,
+          weight:weight,
+          height:height,
+          status:"Experience"
+    })
+  })
+})
 
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
 .get('/', (req, res) => res.render('pages/index'))
-.get('/begginer', (req,res) => res.render('pages/questionarie.ejs'))
-.get('/experience', (req,res) => res.render('pages/proquestion.ejs'))
-// .get('/1', function(req,res){
-//   var loginname = req.body.username;
-//   var password = req.body.pw;
-//   pool.query("SELECT * from user_info WHERE username = '" + loginname + "'", (err,ans) =>{
-//     if ( ans.rowCount == 0)
-//     {
-//       res.render('pages/errorPage', {
-//         msg: "No exsit account"
-//       })
-//     } else if ( ans.rowCount == 1 )
-//     {
-//       if ( password == ans.rows[0].password)
-//       {
-//         var id = ans.rows[0].id
-//         res.render('pages/errorPage', {
-//           msg: "CORRECT :)"
-//         })
-//       } else
-//       {
-//         res.render('pages/errorPage', {
-//           msg: "OH SHIT !"
-//         })
-//       }
-//     }
-//   })
-// })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
