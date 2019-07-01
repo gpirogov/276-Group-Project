0a46aebce656eb6dc66e86d593e45975cec6995c
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-30 17:14:49 -0700

    minor css changes to workout page. fixed 1 rep max js function.

--- public/js/workout.js
+++ public/js/workout.js
@@ -5,20 +5,23 @@ var oneRepMaxButtons = [];	//global vars
 var oneRepMaxForms = [];
 
 
+function toggleElementDisplay(elem) {
+	if(elem.style.display == "none"){
+		elem.style.display="initial";
+	}else{
+		elem.style.display="none";
+	}
+}
+
 window.addEventListener("load", function(){
 
 	oneRepMaxForms = document.getElementsByClassName("one-rep-max-weight-form");
 	oneRepMaxButtons = document.getElementsByClassName("one-rep-max-button");
-	for(var i = 0; i < oneRepMaxForms.length; i++){
-		var currentForm = oneRepMaxForms[i];
-		currentForm.style.display="none";
-		
-		oneRepMaxButtons[i].addEventListener('click', function(){
-			if(currentForm.style.display == "none"){
-				currentForm.style.display="initial";
-			}else{
-				currentForm.style.display="none";
-			}
-		});
+	for(var i = 0; i < oneRepMaxForms.length; i++) {
+		(function () {	// adding this fixed a big bug. solution from https://stackoverflow.com/a/19586183
+			var counter = i;
+			oneRepMaxForms[i].style.display="none";
+			oneRepMaxButtons[i].addEventListener('click', function(){ toggleElementDisplay(oneRepMaxForms[counter]); });
+		}());
 	}
 });
