02d182a3078e10a7a8ca2da47cbdb63f479e48e2
Benny Mai <bmai@sfu.ca>
2019-07-15 11:20:01 -0700

    adjusted html/css and added food search api functionalities

---
+++ public/js/caloriecalc.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+  let goal = 2000 //will be set by questionaire
+  let food= 0 //set by calculating foods
+  let excercise = 0
+  let net = food - excercise
+
+  //api id and key
+  var appId= "83211bdc";
+  var appKey = "af5b66e27d11815d0bb1d0f58651e563";
+
+  function updateCalorieGoal(goal){
+    $('#calorieGoal').text("Daily Calories Goal: " + goal)
+  }
+  function updateCalorieNet(){
+    $('#calorieNet').text("Net Calories: " + net)
+  }
+
+  updateCalorieGoal()
+  updateCalorieNet()
+
+  // set Date
+  today = new Date();
+  let day = today.getDate()
+  let monthNum = today.getMonth()//Jan = 0, Dec = 11
+  let year = today.getFullYear()
+
+  //changing monthNum to monthNames
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let month = monthNames[monthNum]
+
+  $('#date').html(month + " " + day.toString() + " " + year.toString())
+  // document.getElementById("date").innerHTML = DATE
+
+  //toggle add meal page
+  $('#addMealButton').on("click",function(){
+    $('#dietMain').hide()
+    $('#addMeal').show()
+  })
+  //return to main diet page
+  $('#backButton').on("click",function(){
+    $('#dietMain').show()
+    $('#addMeal').hide()
+  })
+
+//api search for food
+  $('#apiSearchButton').on("click",function(){
+    let selectedMeal = $('#mealSelector :selected').val()
+    let food = $('#searchBar').val()
+    console.log(food)
+    $.ajax({
+      type:"GET",
+      url:"https://api.edamam.com/api/food-database/parser",
+      data:{
+        ingr: food,
+        app_id: appId,
+        app_key: appKey
+      },
+      success: function(res){
+        console.log(res);
+        var foodName = res.hints[0].food.label
+        var cals = res.hints[0].food.nutrients.ENERC_KCAL
+        var carbs = res.hints[0].food.nutrients.CHOCDF
+        var fat = res.hints[0].food.nutrients.FAT
+        var protien = res.hints[0].food.nutrients.PROCNT
+        console.log({foodName, cals, carbs, fat, protien});
+
+        //add to tableAddMeal
+        let tableAddMeal = document.getElementById('tableAddMeal')
+        let row = tableAddMeal.insertRow(1)
+        let cell0 = row.insertCell(0)
+        let cell1 = row.insertCell(1)
+        let cell2 = row.insertCell(2)
+        let cell3 = row.insertCell(3)
+        let cell4 = row.insertCell(4)
+        cell0.innerHTML = foodName
+        cell1.innerHTML = cals
+        cell2.innerHTML = fat
+        cell3.innerHTML = carbs
+        cell4.innerHTML = protien
+        }
+      })
+    })
+
+// // add meal to database NEEDS WORK
+// $('#addButton').on("click", function(){
+//   assert(selectedMeal != "Select Meal") //ensures selector is not left in default
+//   //loop table to add to db
+//
+// })
+
+
+  // $('#apiTestButton').on("click",function(){
+  //   $.ajax({
+  //     type:"GET",
+  //     url:"https://api.edamam.com/api/food-database/parser",
+  //     data:{
+  //       ingr: "ham sandwich",
+  //       app_id: appId,
+  //       app_key: appKey
+  //     },
+  //     success: function(res){
+  //       console.log(res);
+  //     }
+  //
+  //   })
+  // })
+
+})
