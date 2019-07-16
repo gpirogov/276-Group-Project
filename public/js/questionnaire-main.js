30884d93bda01eb2df4b456ff0e4cfe53a31ac60
Gleb Pirogov <gpirogov@sfu.ca>
2019-07-15 23:43:38 -0700

    started making questionnaire-main page. will finish tomorrow morning

---
+++ public/js/questionnaire-main.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+window.addEventListener("load", function(){
+	/*document.getElementById("advanced-radio").addEventListener("click", function(){
+	  document.getElementById("skip-questionnaire-row").hidden = false;
+	});
+	document.getElementById("intermediate-radio").addEventListener("click", function(){
+	  document.getElementById("skip-questionnaire-row").hidden = true;
+	  document.getElementById("skip-questionnaire-checkbox").checked = false;
+	});
+	document.getElementById("beginner-radio").addEventListener("click", function(){
+	  document.getElementById("skip-questionnaire-row").hidden = true;
+	  document.getElementById("skip-questionnaire-checkbox").checked = false;
+	});*/
+});
